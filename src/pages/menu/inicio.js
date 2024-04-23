c19521ae649a81e96028c59e2b7931594275d145
FacuAllende <tympo.99@gmail.com>
2024-04-23 15:50:25 -0300

    creando carpetas y nueva pagina inicio

---
+++ src/pages/menu/inicio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './inicio.css';
+
+function Inicio() {
+  return (
+    <div className="Inicio">
+      <h1>Universo Auditivo</h1>
+    </div>
+  );
+}
+
+export default Inicio;
